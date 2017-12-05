de7ec49722761a2074d6e5f65c16c717714e7c59
wbigelow <wbigelow000@gmail.com>
2017-12-04 21:38:52 -0800

    Added two common functions, commented out failing functions

--- scripts/common.R
+++ scripts/common.R
@@ -8,6 +8,31 @@ category.data <- function(category) {
   return(category_data)
 }
 
+# Takes in a vector of major names, returns a data frame containing just those majors.
+filterByMajorName <- function(data, majors) {
+    filteredData <- data.frame()
+    for(major in majors) {
+        filteredData <- rbind(filteredData, data[which(data$Major == major),])
+    }
+    return(filteredData)
+}
+
+# Takes in a dataframe, category AS STRING, and category label (y-axis label) AS STRING, returns a bar graph of Major vs. Category
+# Example call: graphCategory(data, "Median", "Median Pay)
+graphCategory <- function(data, category, categoryLabel) {
+    data["Unemployment_rate"] <- data["Unemployment_rate"] * 100
+    result <- ggplot(data, aes(Major, get(category))) + geom_col(aes(fill = Major_category))
+    result <- result + labs(x = "Major", y = categoryLabel, fill = "Category") + scale_x_discrete( labels = 
+       function(labels) {
+           fixedLabels <- c() 
+           for (l in 1:length(labels)) {
+               fixedLabels <- c(fixedLabels, paste0(ifelse(l %% 2 == 0, '', '\n'), labels[l]))
+           } 
+           return( fixedLabels )
+       })
+    return(result)
+}
+
 1#categories 
 2#Agriculture & Natural Resources
 3#Biology & Life Science
@@ -32,26 +57,23 @@ categories <- c("Agriculture & Natural Resources", "Biology & Life Science","Eng
                 "Interdisciplinary","Health","Social Science","Physical Sciences","Psychology & Social Work",
                 "Arts","Business")
 
-for(category in c("Agriculture & Natural Resources", "Biology & Life Science","Engineering", "Humanities & Liberal Arts",
-                  "Communications & Journalism", "Computers & Mathematics",
-                  "Industrial Arts & Consumer Services", "Education","Law & Public Policy", 
-                  "Interdisciplinary","Health","Social Science","Physical Sciences","Psychology & Social Work",
-                  "Arts","Business")) {
-  data <- category.data(category)
-  pay <- 
-  unemployment_rate <- 
-  
-}
+# for(category in categories) {
+#   data <- category.data(category)
+#   pay <- NULL
+#   unemployment_rate <- NULL
+# }
 
 
 employment.rate <- function(category_or_major, data_to_filter) {
   
 }
 
-pay.aggregate.data <- aggregate(data, by = list("Agriculture & Natural Resources", "Biology & Life Science","Engineering", "Humanities & Liberal Arts",
-  "Communications & Journalism", "Computers & Mathematics","Industrial Arts & Consumer Services", "Education","Law & Public Policy", 
-  "Interdisciplinary","Health","Social Science","Physical Sciences","Psychology & Social Work", "Arts","Business"), FUN = mean,
-  na.rm = TRUE)
+
+# pay.aggregate.data <- aggregate(data, by = list("Agriculture & Natural Resources", "Biology & Life Science","Engineering", "Humanities & Liberal Arts",
+#   "Communications & Journalism", "Computers & Mathematics","Industrial Arts & Consumer Services", "Education","Law & Public Policy", 
+#   "Interdisciplinary","Health","Social Science","Physical Sciences","Psychology & Social Work", "Arts","Business"), FUN = mean,
+#   na.rm = TRUE)
+
 
 
 
