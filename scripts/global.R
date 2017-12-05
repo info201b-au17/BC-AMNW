add04aa50fdcc0ec214e812444ec587ca503b1ce
Miguel <mayorga@uw.edu>
2017-12-04 22:49:08 -0800

    what I have so far

---
+++ scripts/global.R
@@ -0,0 +1,19 @@
+
+MajorCategory <- c("Agriculture & Natural Resources" = "Agriculture & Natural Resources",
+                    "Biology & Life Science" = "Biology & Life Science",
+                    "Engineering" = "Engineering",
+                    "Humanities & Liberal Arts" = "Humanities & Liberal Arts",
+                    "Communications & Journalism" = "Communications & Journalism",
+                    "Computers & Mathematics" = "Computers & Mathematics",
+                    "Industrial Arts & Consumer Services" = "Industrial Arts & Consumer Services",
+                    "Education" = "Education",
+                    "Law & Public Policy" = "Law & Public Policy",
+                    "Interdisciplinary" = "Interdisciplinary",
+                    "Health" = "Health",
+                    "Social Science" = "Social Science",
+                    "Physical Sciences" = "Physical Sciences",
+                    "Psychology & Social Work" = "Psychology & Social Work",
+                    "Arts" = "Arts",
+                    "Business" = "Business")
+
+selectGraph <- "place graphs here"
--- scripts/ui.R
+++ scripts/ui.R
@@ -1,2 +1,20 @@
 library(shiny)
 library(rsconnect)
+
+pageWithSidebar(
+  titlePanel("Manufacturer & Category"),
+  sidebarPanel(
+    
+    selectInput("Categories", label = h3("Major Category:"),
+                choices = MajorCategory),
+    
+    selectInput("Type", label = h3("Graph Type:"),
+                choices = selectGraph)
+  ),
+  
+  mainPanel(
+    plotOutput("plot1")
+  )
+)
+
+
